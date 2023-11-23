fc83cf26eca9cc152863acb5127bccc840e74c91
AryanOnPc <aryan.k.randeriya@gmail.com>
2023-11-23 17:33:12 +0530

    update CSS/index.css, software_projects.css, software_projects.html and software_projects.js

--- JS/software_projects.js
+++ JS/software_projects.js
@@ -20,45 +20,50 @@ initializeApp(firebaseConfig);
 const database = getDatabase();
 const projectsRef = ref(database, "Projects/");
 
-onValue(
-  projectsRef,
-  (snapshot) => {
-    snapshot.forEach((childSnapshot) => {
-      const projectDetails = childSnapshot.val();
-      console.log("Project ID: ", childSnapshot.key);
+// ... Your existing code
 
-      var projectTitle = projectDetails.project_title;
-      var projectDesc = projectDetails.project_description;
-      var projectType = projectDetails.project_type;
-      var githubLink = projectDetails.github_link;
+function processDataAndHidePreloader(snapshot) {
+  snapshot.forEach((childSnapshot) => {
+    const projectDetails = childSnapshot.val();
+    console.log("Project ID: ", childSnapshot.key);
 
-      console.log("Project Desc:", projectDesc);
-      console.log("Project Title:", projectTitle);
-      console.log("Project Type:", projectType);
-      console.log("Github Link:", githubLink);
+    var projectTitle = projectDetails.project_title;
+    var projectDesc = projectDetails.project_description;
+    var projectType = projectDetails.project_type;
+    var githubLink = projectDetails.github_link;
 
-      const card = document.createElement("div");
-      card.innerHTML = `
-        <div class="card" id="card">
-          <div class="image"></div>
-            <div class="content">
-                <a href="#"> <span class="card_title">${projectTitle}</span></a>
-                <p class="card_desc">${projectDesc} </p>
+    console.log("Project Desc:", projectDesc);
+    console.log("Project Title:", projectTitle);
+    console.log("Project Type:", projectType);
+    console.log("Github Link:", githubLink);
 
-                <div class="btns">
-                    <a class="action secondary" href="${githubLink}" target="_blank">Github Repo<span aria-hidden="true" class="icon_btn"> <i
-                                class="fa-brands fa-github fa-2x" style="color: #999999;"></i> </span></a>
+    const card = document.createElement("div");
+    card.innerHTML = `
+      <div class="card" id="card">
+        <div class="image"></div>
+          <div class="content">
+              <a href="#"> <span class="card_title">${projectTitle}</span></a>
+              <p class="card_desc">${projectDesc} </p>
 
-                    <a class="action primary" href="#">View Project<span aria-hidden="true" class="icon_btn">
-                            <span class="material-symbols-outlined fa-1x">open_in_new</span></a>
-                </div>
-            </div>
-        </div>
-        
-        `;
+              <div class="btns">
+                  <a class="action secondary" href="${githubLink}" target="_blank">Github Repo<span aria-hidden="true" class="icon_btn"> <i
+                              class="fa-brands fa-github fa-2x" style="color: #999999;"></i> </span></a>
 
-      document.getElementById("cards").appendChild(card);
-    });
-  },
-  { onlyOnce: true }
-);
+                  <a class="action primary" href="#">View Project<span aria-hidden="true" class="icon_btn">
+                          <span class="material-symbols-outlined fa-1x">open_in_new</span></a>
+              </div>
+          </div>
+      </div>
+    `;
+
+    document.getElementById("cards").appendChild(card);
+  });
+
+  const preloader = document.querySelector(".preloader");
+  const content = document.getElementById("content");
+
+  preloader.style.display = "none";
+  content.style.display = "block";
+}
+
+onValue(projectsRef, processDataAndHidePreloader, { onlyOnce: true });

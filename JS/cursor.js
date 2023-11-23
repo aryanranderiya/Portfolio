56f6c2c7eeaf90299869147ae7ebf026a174efe1
AryanOnPc <aryan.k.randeriya@gmail.com>
2023-11-23 22:55:23 +0530

    update cursor.css, CSS/index.css, software_projects.html and cursor.js

--- JS/cursor.js
+++ JS/cursor.js
@@ -1,9 +1,11 @@
 document.addEventListener("mousedown", function () {
   document.body.style.cursor =
-    'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" stroke="%23ffffff" fill="%23ffffff" width="20px" height="20px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\') 10 10, auto';
+    'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23ffffff" stroke="%23ffffff" width="70px" height="70px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\') 10 10, auto';
 });
 
 document.addEventListener("mouseup", function () {
   document.body.style.cursor =
-    'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="%23ffffff" width="20px" height="20px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\') 10 10, auto';
+    'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23ffffff" stroke="%23ffffff" width="20px" height="20px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\')10 10,auto';
 });
+
+//   document.body.classList.add("invert-filter");

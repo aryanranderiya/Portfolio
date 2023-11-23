a709ee2c175fd59b18bbe78d509113ce18a7fcf5
AryanOnPc <aryan.k.randeriya@gmail.com>
2023-11-23 15:19:55 +0530

    update admin_panel.html, admin_panel.js and firebase_init.js

---
+++ JS/admin_panel.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUfVSJ3BpDrlV7-6Ou9DWE3KX1jHHFGEY",
+  authDomain: "aryanranderiya-portfolio.firebaseapp.com",
+  projectId: "aryanranderiya-portfolio",
+  storageBucket: "aryanranderiya-portfolio.appspot.com",
+  messagingSenderId: "540599201791",
+  appId: "1:540599201791:web:757239eec07a33dc9ec1ca",
+  measurementId: "G-S3XP36W3P9",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase();
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("submit-project-form")
+    .addEventListener("submit", function (e) {
+      console.log("DOMContentLoaded Event listener called");
+
+      e.preventDefault();
+
+      const title = document.getElementById("project_title").value;
+      const type = document.getElementById("project_type").value;
+      const description = document.getElementById("project_description").value;
+      const githubLink = document.getElementById("githubLink").value;
+
+      const project_id = "project_" + title + "_" + Date.now();
+      var projectsRef = ref(database, "Projects/" + project_id);
+
+      set(projectsRef, {
+        project_title: title,
+        project_type: type,
+        project_description: description,
+        github_link: githubLink,
+      })
+        .then(() => {
+          console.log("Successfully inserted!");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      document.getElementById("submit-project-form").reset();
+    });
+});
--- JS/firebase_init.js
+++ JS/firebase_init.js
@@ -1,12 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAUfVSJ3BpDrlV7-6Ou9DWE3KX1jHHFGEY",
   authDomain: "aryanranderiya-portfolio.firebaseapp.com",
@@ -17,6 +10,4 @@ const firebaseConfig = {
   measurementId: "G-S3XP36W3P9",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);

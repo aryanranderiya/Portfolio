ce712e10c3a67cb67f8b0d54dbd4602f0d29e22d
Aryan <aryanranderiya1478@gmail.com>
2023-11-27 20:30:23 +0530

    added all the files again

---
+++ JS/cursor.js
@@ -0,0 +1,43 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const textElement1 = document.querySelector(".aryan_randeriya");
+
+//   textElement1.addEventListener("mouseover", () => {
+//     // document.body.style.cursor =
+//     //   'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23ffffff" stroke="%23ffffff" width="70px" height="70px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\') 10 10, auto';
+
+//     document.body.style.cursor =
+//       'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="rgba(255, 255, 255, 1)" width="70px" height="70px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\') 10 10, auto';
+//   });
+
+//   textElement1.addEventListener("mouseout", () => {
+//     document.body.style.cursor =
+//       'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23ffffff" stroke="%23ffffff" width="20px" height="20px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\')10 10,auto';
+//   });
+
+//   const textElement2 = document.querySelector(".my_portfolio");
+
+//   textElement2.addEventListener("mouseover", () => {
+//     document.body.style.cursor =
+//       'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="rgba(255, 255, 255, 1)" width="70px" height="70px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\') 10 10, auto';
+//   });
+
+//   textElement2.addEventListener("mouseout", () => {
+//     document.body.style.cursor =
+//       'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23ffffff" stroke="%23ffffff" width="20px" height="20px" viewBox="0 0 10.04 10.04"><circle cx="5.02" cy="5.02" r="4.52"/></svg>\')10 10,auto';
+//   });
+// });
+
+// ! New Cursor
+
+const cursor = document.querySelector(".cursor");
+
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const software_projects_title = document.getElementById(
+  "software_projects_title"
+);
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
---
+++ JS/firebase_init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUfVSJ3BpDrlV7-6Ou9DWE3KX1jHHFGEY",
+  authDomain: "aryanranderiya-portfolio.firebaseapp.com",
+  projectId: "aryanranderiya-portfolio",
+  storageBucket: "aryanranderiya-portfolio.appspot.com",
+  messagingSenderId: "540599201791",
+  appId: "1:540599201791:web:757239eec07a33dc9ec1ca",
+  measurementId: "G-S3XP36W3P9",
+};
+
+const app = initializeApp(firebaseConfig);
